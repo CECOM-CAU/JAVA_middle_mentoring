731c2a477d118d6b8d879077eceb5d5396bc0ffc
splo2t <yu990410@naver.com>
2019-04-08 18:49:08 +0900

    initial study

---
+++ src/Direction.java
@@ -0,0 +1,13 @@
+class Direction{
+	int x = 0;
+	int y = 0;
+	int z = 0;
+	int size = 0;
+	
+	Direction(int x, int y, int z, int size){
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		this.size = size;
+	}
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/* Splo2t_현욱*/
+public class Main{
+	public static void main(String[] args) {
+		
+		Jungeojin 어진밴 = new Jungeojin();
+		Direction d = new Direction(10,10,10,10000000000000);
+		어진밴.move(d);
+		
+	}
+}
