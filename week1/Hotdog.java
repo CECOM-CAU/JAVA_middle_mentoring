c87ef5cb6af88bbd3591bca59b6a1ecd274ab3ca
eojindr <44453643+eojindr@users.noreply.github.com>
2019-04-29 18:48:32 +0900

    Add files via upload

---
+++ week1/Hotdog.java
@@ -0,0 +1,13 @@
+package hwbrz2;
+
+public class Hotdog extends Snack {
+	
+	String content;
+	boolean sugar_exis;
+	boolean ketchup_exis;
+	
+	
+	
+	
+	
+}
---
+++ week1/Sausages.java
@@ -0,0 +1,5 @@
+package hwbrz2;
+
+public class Sausages {
+
+}
